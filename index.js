9e4c080255b5fbcd5525da340ee35fe479b178e9
Zelvya <25373760+Zelvya@users.noreply.github.com>
2019-02-17 07:19:40 +0700

    update index.html, add index.css, add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+/** Set current date to the "Current date:" label area **/
+const letsSetCurrentDate = () => { }
+
+/** Calculate the number of months between current date and target date **/
+const letsCalculateRemainingMonths = (currentDate, targetDate) => {}
+
+/** Calculate the number of weeks between current date and target date **/
+const letsCalculateRemainingWeeks = (currentDate, targetDate) => {}
+
+/** Calculate the number of days between current date and target date **/
+const letsCalculateRemainingDays = (currentDate, targetDate) => {}
+
+/** Calculate the number of hours between current date and target date **/
+const letsCalculateRemainingHours = (currentDate, targetDate) => {}
+
+/** Calculate the monthly load value **/
+const letsCalculateMonthlyLoad = (totalAmount, remainingMonths) => {}
+
+/** Calculate the weekly load value **/
+const letsCalculateWeeklyLoad = (totalAmount, remainingWeeks) => {}
+
+/** Calculate the daily load value **/
+const letsCalculateDailyLoad = (totalAmount, remainingDays) => {}
+
+/** Calculate the hourly load value **/
+const letsCalculateHourlyLoad = (totalAmount, remainingHours) => {}
